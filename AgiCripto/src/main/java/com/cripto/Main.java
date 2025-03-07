204ee1bf74ac7718e121cf05c953c737f720244e
samuel.moreira <samuel442005@gmail.com>
2025-03-07 08:52:28 -0300

    criação da branch

--- AgiCripto/src/main/java/com/cripto/Main.java
+++ AgiCripto/src/main/java/com/cripto/Main.java
@@ -9,16 +9,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-<<<<<<< HEAD
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite 0 para conexao online e 1 para local: ");
         int opcao = scanner.nextInt();
-
         Conexao conexao = new Conexao();
         conexao.getConexao(opcao);
-=======
         ClienteController controller = new ClienteController();
         controller.cadastro();
->>>>>>> 5adc420d9f10b90e3e811aa9ba09a9369d3d0e79
     }
 }
