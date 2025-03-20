4baacd8e65b1423aec03480da446665aa0fae146
Vitor Coelho <vijcoelho@gmail.com>
2025-03-20 11:23:15 -0300

    Metodo assinatura completo

--- AgiCripto/src/main/java/com/cripto/Main.java
+++ AgiCripto/src/main/java/com/cripto/Main.java
@@ -1,11 +1,9 @@
 package com.cripto;
 
+import com.cripto.controller.AssinaturaController;
 import com.cripto.controller.CarteiraCriptoController;
 import com.cripto.controller.ClienteController;
-import com.cripto.dao.CarteiraCriptoDAO;
-import com.cripto.dao.CarteiraDAO;
-import com.cripto.dao.ClienteDAO;
-import com.cripto.dao.TransacaoDAO;
+import com.cripto.dao.*;
 import com.cripto.model.database.Conexao;
 import com.cripto.view.CarteiraCriptoView;
 import com.cripto.view.ClienteView;
@@ -39,6 +37,8 @@ public class Main {
         TransacaoDAO transacaoDAO = new TransacaoDAO(connection);
         ClienteController controller = new ClienteController(clienteDAO, carteiraDAO, transacaoDAO);
         CarteiraCriptoDAO criptoDAO = new CarteiraCriptoDAO(connection);
+        AssinaturaDAO assinaturaDAO = new AssinaturaDAO(connection);
+        AssinaturaController assinaturaController = new AssinaturaController(controller, assinaturaDAO);
         CarteiraCriptoController carteiraCriptoController = new CarteiraCriptoController(
                 controller,
                 criptoDAO,
@@ -46,7 +46,7 @@ public class Main {
                 clienteDAO,
                 transacaoDAO
         );
-        CarteiraCriptoView carteiraCriptoView = new CarteiraCriptoView(carteiraCriptoController, controller, carteiraDAO);
+        CarteiraCriptoView carteiraCriptoView = new CarteiraCriptoView(carteiraCriptoController, controller, carteiraDAO, assinaturaController);
         return new ClienteView(controller, carteiraDAO, carteiraCriptoController, carteiraCriptoView, carteiraCriptoDAO);
     }
 }
