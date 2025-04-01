b137517379b04c9b0d1541b7e8cc866beb6a33b4
Vitor Coelho <vijcoelho@gmail.com>
2025-04-01 10:51:30 -0300

    Squashed 'agiJavaFx/' content from commit 8d53b8f
    git-subtree-dir: agiJavaFx
    git-subtree-split: 8d53b8fd78364b9f83c5c10d7bef5b2e66d7739c

---
+++ agi/src/main/java/com/cripto/agi/agi/javafx/controllers/PixController.java
@@ -0,0 +1,105 @@
+package com.cripto.agi.agi.javafx.controllers;
+
+import com.cripto.agi.agi.controller.CarteiraCriptoController;
+import com.cripto.agi.agi.controller.ClienteController;
+import com.cripto.agi.agi.dao.CarteiraDAO;
+import com.cripto.agi.agi.model.Carteira;
+import com.cripto.agi.agi.model.Cliente;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PixController {
+    @FXML
+    public Label saldoLabel;
+    @FXML
+    public Label nomeLabel;
+    @FXML
+    public TextField pixText;
+
+    private ClienteController controller;
+    private CarteiraDAO carteiraDAO;
+    private CarteiraCriptoController carteiraCriptoController;
+
+    public void setClienteController(ClienteController controller, CarteiraDAO carteiraDAO, CarteiraCriptoController carteiraCriptoController) {
+        this.controller = controller;
+        this.carteiraDAO = carteiraDAO;
+        this.carteiraCriptoController = carteiraCriptoController;
+        this.carregarInfos();
+    }
+
+    public void carregarInfos() {
+        Cliente cliente = controller.pegarClienteLogado();
+        Carteira carteira = carteiraDAO.pegarCarteiraPeloClienteId(cliente.getId_cliente());
+
+        nomeLabel.setText(cliente.getNome());
+        saldoLabel.setText(String.valueOf(carteira.getSaldoContaCorrente()));
+    }
+
+    public void fazerPix(ActionEvent actionEvent) throws IOException {
+        String valor = pixText.getText();
+        Double valorEmDouble = Double.valueOf(valor);
+
+        if (controller.comprar(valorEmDouble)) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Pix realizado");
+            alert.setHeaderText(null);
+            alert.setContentText("Pix realizado com sucesso!");
+            alert.showAndWait();
+            voltarParaCarteira(actionEvent);
+        } else {
+            System.out.println("Pix nao realizado!");
+        }
+    }
+
+    public void voltarParaCarteira(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/cripto/agi/agi/carteiraCorrente.fxml"));
+        Parent root = loader.load();
+
+        CarteiraCorrenteController carteiraCorrenteController = loader.getController();
+        carteiraCorrenteController.setClienteController(this.controller, this.controller.getCarteiraDAO(), carteiraCriptoController);
+
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+    }
+
+    public void carteiraCripto(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/cripto/agi/agi/carteiraCripto.fxml"));
+        Parent root = loader.load();
+
+        Cliente cliente = controller.pegarClienteLogado();
+        if (cliente.getStatus().equals("desativado")) {
+            carteiraCriptoController.ativarCarteiraCripto();
+        }
+
+        CriptoController criptoController = loader.getController();
+        criptoController.setClienteController(this.controller, this.carteiraDAO, this.carteiraCriptoController);
+
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+    }
+
+    public void sair(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/cripto/agi/agi/login.fxml"));
+        Parent root = loader.load();
+
+        LoginController loginController = loader.getController();
+        loginController.setClienteController(controller, carteiraDAO, carteiraCriptoController);
+
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+    }
+}
